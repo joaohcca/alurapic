935b37e702277d1732723689e6cca89366a15d46
João Henrique Albuquerque <joaohccalbu@gmail.com>
2022-07-22 19:01:24 -0300

    adicionando output properties e font-awesome

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,19 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CardModule } from 'src/app/shared/components/card/card.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filterByDescription.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     PhotoListComponent,
     PhotosComponent,
     LoadButtonComponent,
-    FilterByDescription
+    FilterByDescription,
+    SearchComponent
   ],
 
-  imports: [CommonModule, PhotoModule]
+  imports: [
+    CommonModule,
+    PhotoModule,
+    CardModule
+  ]
 })
 export class PhotoListModule { }
