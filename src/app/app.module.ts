47a5d22307ec75560711bb28f83f9a09d009bd34
João Henrique Albuquerque <joaohccalbu@gmail.com>
2022-07-22 14:48:44 -0300

    finalizando primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
